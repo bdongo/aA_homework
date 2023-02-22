84edaffa190d395237a76167c0604e0bc6f11f2e
brandon <brandon.w.leung@gmail.com>
2023-02-21 21:07:57 -0800

    homework

---
+++ wk4d2/practice-for-ch-ruby-simon-main/lib/simon.rb
@@ -0,0 +1,55 @@
+class Simon
+  COLORS = %w(red blue green yellow)
+
+  attr_accessor :sequence_length, :game_over, :seq
+
+  def initialize
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
+  end
+
+  def play
+    self.take_turn
+    until game_over == true
+      self.take_turn
+    end
+    self.game_over_message
+    self.reset_game
+  end
+
+  def take_turn
+    self.show_sequence
+    self.require_sequence
+    if game_over == false
+      self.round_success_message
+      @sequence_length += 1
+    end
+  end
+
+  def show_sequence
+    self.add_random_color
+    seq.each do |color|
+      puts color
+    end
+  end
+
+  def require_sequence
+  end
+
+  def add_random_color
+    seq << COLORS.sample
+  end
+
+  def round_success_message
+  end
+
+  def game_over_message
+  end
+
+  def reset_game
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
+  end
+end
